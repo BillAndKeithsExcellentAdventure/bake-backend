0cd4211c03c75d31a5597d9c816be2e2b3449751
KeithBertram <keith.m.bertram@gmail.com>
2025-01-10 16:21:22 -0500

    updates

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,7 +5,7 @@ export const errorHandler = (err, req, res, next) => {
   console.error(err);
   res.status(err.status || 500).json({
     message: err.message || 'Something went wrong',
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: config.nodeEnv === 'production' ? null : err.stack,
   });
 };
 
